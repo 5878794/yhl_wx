c36680477a2726dfa6cb9435c13210acc67ed9bc
bens <5878794@qq.com>
2020-07-26 14:45:32 +0800

    add recover

---
+++ miniprogram/pages/recover/index.js
@@ -0,0 +1,21 @@
+//index.js
+const app = getApp();
+// import server from '../../lib/server.js';
+// import {ajax,api} from '../../lib/ajax.js';
+import sys from '../../lib/sys.js';
+import $ from '../../lib/jq.js';
+
+
+Page({
+	data: {
+
+	},
+
+	onLoad: function() {
+
+
+
+
+	}
+
+});
